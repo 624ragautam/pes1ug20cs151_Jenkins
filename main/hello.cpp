0e3df46e7bbfd8101128d6b259b116884c799239
Gautam.R.A <gautamra624@gmail.com>
2023-02-17 19:42:10 +0530

    Second commit

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "THIS IS GAUTAM PES1UG20CS151";
+  cout << "THIS IS GAUTAM.R.A PES1UG20CS151";
   cout << "I have successfully built and run\n";
   return 0;
 }
